e2e9f163e0c63fb1e05f58bfa08ef962404d3054
GerryMG <gerardo.andresz4@live.com>
2020-04-19 22:10:02 -0600

    model usuario change

--- models/usuario.js
+++ models/usuario.js
@@ -4,25 +4,28 @@ var Schema = mongoose.Schema;
 
 var usuarioSchema = new Schema(
     {
-        dui: {type: String, unique: true, require: true},
-        nombre: {type: String, required: true},
-        peso: {type: Number, required: true},
-        nacimiento: {type: Date, required: true},
-        altura:{type: Number, required: true},
-        pais: {type: String, required: true},
-        cuidad: {type: String, required: true},
-        sexo:{type: String, required: true},
-        email: {type: String, unique: true, required: true},
-        password: {type: String, required: true}, 
-        tags:[{type:String}],
-        saldo:{type: Number, required: true},
-        ip_disp: [{type: String}],
-        
-        carrito:[{type: String}],
-        cant_personas: {type: Number, require: true},
-        publico: [{type: String, required: true}],
-        costo: {type: Number, required: true},
-        descuento: {type: String}
+        dui: { type: String, unique: true, require: true },
+        nombre: { type: String, required: true },
+        peso: { type: Number, required: true },
+        nacimiento: { type: Date, required: true },
+        altura: { type: Number, required: true },
+        pais: { type: String, required: true },
+        cuidad: { type: String, required: true },
+        sexo: { type: String, required: true },
+        email: { type: String, unique: true, required: true },
+        password: { type: String, required: true },
+        tags: [{ type: String }],
+        saldo: { type: Number, required: true },
+        ip_disp: [{ type: String }],
+
+        carrito: [{
+            id_encuesta: {type: String, ref: "encuesta"},
+            cant_personas: { type: Number, require: true },
+            publico: [{ type: String, required: true }],
+            costo: { type: Number, required: true },
+            descuento: { type: String }
+        }],
+
     }
 );
 
