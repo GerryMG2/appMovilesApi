ba4295245f05ef21e031ac7062c59ba76875139f
GerryMG <gerardo.andresz4@live.com>
2020-05-19 04:19:01 -0600

    apit transaccional auth

--- routes/apiTransaccional.js
+++ routes/apiTransaccional.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var auth = require("../security/adminsecurity/auth");
 
 const ciudadController = require("../controllers/api_controllers_transaccional/ciudadController");
 const ciudad = new ciudadController();
@@ -55,104 +55,104 @@ const usuarioController = require("../controllers/api_controllers_transaccional/
 const usuario = new usuarioController();
 
 
-router.get("/ciudad", ciudad.get);
-router.get("/ciudad/id", ciudad.getOneById);
-router.post("/ciudad", ciudad.create);
-router.delete("/ciudad", ciudad.delete);
-router.put("/ciudad", ciudad.update);
-router.put("/ciudad/upsert", ciudad.updateOrCreate);
+router.get("/ciudad",auth, ciudad.get);
+router.get("/ciudad/id",auth, ciudad.getOneById);
+router.post("/ciudad",auth, ciudad.create);
+router.delete("/ciudad",auth, ciudad.delete);
+router.put("/ciudad",auth, ciudad.update);
+router.put("/ciudad/upsert",auth, ciudad.updateOrCreate);
 
-router.get("/cliente", cliente.get);
-router.get("/cliente/id", cliente.getOneById);
-router.post("/cliente", cliente.create);
-router.delete("/cliente", cliente.delete);
-router.put("/cliente", cliente.update);
-router.put("/cliente/upsert", cliente.updateOrCreate);
+router.get("/cliente",auth, cliente.get);
+router.get("/cliente/id",auth, cliente.getOneById);
+router.post("/cliente",auth, cliente.create);
+router.delete("/cliente",auth, cliente.delete);
+router.put("/cliente",auth, cliente.update);
+router.put("/cliente/upsert",auth, cliente.updateOrCreate);
 
-router.get("/cuenta", cuenta.get);
-router.get("/cuenta/id", cuenta.getOneById);
-router.post("/cuenta", cuenta.create);
-router.delete("/cuenta", cuenta.delete);
-router.put("/cuenta", cuenta.update);
-router.put("/cuenta/upsert", cuenta.updateOrCreate);
+router.get("/cuenta",auth, cuenta.get);
+router.get("/cuenta/id",auth, cuenta.getOneById);
+router.post("/cuenta",auth, cuenta.create);
+router.delete("/cuenta",auth, cuenta.delete);
+router.put("/cuenta",auth, cuenta.update);
+router.put("/cuenta/upsert",auth, cuenta.updateOrCreate);
 
 
-router.get("/descuento", descuento.get);
-router.get("/descuento/id", descuento.getOneById);
-router.post("/descuento", descuento.create);
-router.delete("/descuento", descuento.delete);
-router.put("/descuento", descuento.update);
-router.put("/descuento/upsert", descuento.updateOrCreate);
+router.get("/descuento",auth, descuento.get);
+router.get("/descuento/id",auth, descuento.getOneById);
+router.post("/descuento",auth, descuento.create);
+router.delete("/descuento",auth, descuento.delete);
+router.put("/descuento",auth, descuento.update);
+router.put("/descuento/upsert",auth, descuento.updateOrCreate);
 
-router.get("/detalleEncuesta", detalleEncuesta.get);
-router.get("/detalleEncuesta/id", detalleEncuesta.getOneById);
-router.post("/detalleEncuesta", detalleEncuesta.create);
-router.delete("/detalleEncuesta", detalleEncuesta.delete);
-router.put("/detalleEncuesta", detalleEncuesta.update);
-router.put("/detalleEncuesta/upsert", detalleEncuesta.updateOrCreate);
+router.get("/detalleEncuesta",auth, detalleEncuesta.get);
+router.get("/detalleEncuesta/id",auth, detalleEncuesta.getOneById);
+router.post("/detalleEncuesta",auth, detalleEncuesta.create);
+router.delete("/detalleEncuesta",auth, detalleEncuesta.delete);
+router.put("/detalleEncuesta",auth, detalleEncuesta.update);
+router.put("/detalleEncuesta/upsert",auth, detalleEncuesta.updateOrCreate);
 
-router.get("/encuestaT", encuestaT.get);
-router.get("/encuestaT/id", encuestaT.getOneById);
-router.post("/encuestaT", encuestaT.create);
-router.delete("/encuestaT", encuestaT.delete);
-router.put("/encuestaT", encuestaT.update);
-router.put("/encuestaT/upsert", encuestaT.updateOrCreate);
+router.get("/encuestaT",auth, encuestaT.get);
+router.get("/encuestaT/id",auth, encuestaT.getOneById);
+router.post("/encuestaT",auth, encuestaT.create);
+router.delete("/encuestaT",auth, encuestaT.delete);
+router.put("/encuestaT",auth, encuestaT.update);
+router.put("/encuestaT/upsert",auth, encuestaT.updateOrCreate);
 
-router.get("/pais", pais.get);
-router.get("/pais/id", pais.getOneById);
-router.post("/pais", pais.create);
-router.delete("/pais", pais.delete);
-router.put("/pais", pais.update);
-router.put("/pais/upsert", pais.updateOrCreate);
+router.get("/pais",auth, pais.get);
+router.get("/pais/id",auth, pais.getOneById);
+router.post("/pais",auth, pais.create);
+router.delete("/pais",auth, pais.delete);
+router.put("/pais",auth, pais.update);
+router.put("/pais/upsert",auth, pais.updateOrCreate);
 
-router.get("/servicio", servicio.get);
-router.get("/servicio/id", servicio.getOneById);
-router.post("/servicio", servicio.create);
-router.delete("/servicio", servicio.delete);
-router.put("/servicio", servicio.update);
-router.put("/servicio/upsert", servicio.updateOrCreate);
+router.get("/servicio",auth, servicio.get);
+router.get("/servicio/id",auth, servicio.getOneById);
+router.post("/servicio",auth, servicio.create);
+router.delete("/servicio",auth, servicio.delete);
+router.put("/servicio",auth, servicio.update);
+router.put("/servicio/upsert",auth, servicio.updateOrCreate);
 
-router.get("/tipoPago", tipoPago.get);
-router.get("/tipoPago/id", tipoPago.getOneById);
-router.post("/tipoPago", tipoPago.create);
-router.delete("/tipoPago", tipoPago.delete);
-router.put("/tipoPago", tipoPago.update);
-router.put("/tipoPago/upsert", tipoPago.updateOrCreate);
+router.get("/tipoPago",auth, tipoPago.get);
+router.get("/tipoPago/id",auth, tipoPago.getOneById);
+router.post("/tipoPago",auth, tipoPago.create);
+router.delete("/tipoPago",auth, tipoPago.delete);
+router.put("/tipoPago",auth, tipoPago.update);
+router.put("/tipoPago/upsert",auth, tipoPago.updateOrCreate);
 
-router.get("/tipotransaccionDoc", tipotransaccionDoc.get);
-router.get("/tipotransaccionDoc/id", tipotransaccionDoc.getOneById);
-router.post("/tipotransaccionDoc", tipotransaccionDoc.create);
-router.delete("/tipotransaccionDoc", tipotransaccionDoc.delete);
-router.put("/tipotransaccionDoc", tipotransaccionDoc.update);
-router.put("/tipotransaccionDoc/upsert", tipotransaccionDoc.updateOrCreate);
+router.get("/tipotransaccionDoc",auth, tipotransaccionDoc.get);
+router.get("/tipotransaccionDoc/id",auth, tipotransaccionDoc.getOneById);
+router.post("/tipotransaccionDoc",auth, tipotransaccionDoc.create);
+router.delete("/tipotransaccionDoc",auth, tipotransaccionDoc.delete);
+router.put("/tipotransaccionDoc",auth, tipotransaccionDoc.update);
+router.put("/tipotransaccionDoc/upsert",auth, tipotransaccionDoc.updateOrCreate);
 
-router.get("/transaccion", transaccion.get);
-router.get("/transaccion/id", transaccion.getOneById);
-router.post("/transaccion", transaccion.create);
-router.delete("/transaccion", transaccion.delete);
-router.put("/transaccion", transaccion.update);
-router.put("/transaccion/upsert", transaccion.updateOrCreate);
+router.get("/transaccion",auth, transaccion.get);
+router.get("/transaccion/id",auth, transaccion.getOneById);
+router.post("/transaccion",auth, transaccion.create);
+router.delete("/transaccion",auth, transaccion.delete);
+router.put("/transaccion",auth, transaccion.update);
+router.put("/transaccion/upsert",auth, transaccion.updateOrCreate);
 
-router.get("/transaccionDetalle", transaccionDetalle.get);
-router.get("/transaccionDetalle/id", transaccionDetalle.getOneById);
-router.post("/transaccionDetalle", transaccionDetalle.create);
-router.delete("/transaccionDetalle", transaccionDetalle.delete);
-router.put("/transaccionDetalle", transaccionDetalle.update);
-router.put("/transaccionDetalle/upsert", transaccionDetalle.updateOrCreate);
+router.get("/transaccionDetalle",auth, transaccionDetalle.get);
+router.get("/transaccionDetalle/id",auth, transaccionDetalle.getOneById);
+router.post("/transaccionDetalle",auth, transaccionDetalle.create);
+router.delete("/transaccionDetalle",auth, transaccionDetalle.delete);
+router.put("/transaccionDetalle",auth, transaccionDetalle.update);
+router.put("/transaccionDetalle/upsert",auth, transaccionDetalle.updateOrCreate);
 
-router.get("/usuarioAdmin", usuarioAdmin.get);
-router.get("/usuarioAdmin/id", usuarioAdmin.getOneById);
-router.post("/usuarioAdmin", usuarioAdmin.create);
-router.delete("/usuarioAdmin", usuarioAdmin.delete);
-router.put("/usuarioAdmin", usuarioAdmin.update);
-router.put("/usuarioAdmin/upsert", usuarioAdmin.updateOrCreate);
+router.get("/usuarioAdmin",auth, usuarioAdmin.get);
+router.get("/usuarioAdmin/id",auth, usuarioAdmin.getOneById);
+router.post("/usuarioAdmin",auth, usuarioAdmin.create);
+router.delete("/usuarioAdmin",auth, usuarioAdmin.delete);
+router.put("/usuarioAdmin",auth, usuarioAdmin.update);
+router.put("/usuarioAdmin/upsert",auth, usuarioAdmin.updateOrCreate);
 
-router.get("/usuario", usuario.get);
-router.get("/usuario/id", usuario.getOneById);
-router.post("/usuario", usuario.create);
-router.delete("/usuario", usuario.delete);
-router.put("/usuario", usuario.update);
-router.put("/usuario/upsert", usuario.updateOrCreate);
+router.get("/usuario",auth, usuario.get);
+router.get("/usuario/id",auth, usuario.getOneById);
+router.post("/usuario",auth, usuario.create);
+router.delete("/usuario",auth, usuario.delete);
+router.put("/usuario",auth, usuario.update);
+router.put("/usuario/upsert",auth, usuario.updateOrCreate);
 
 
 module.exports = router;
