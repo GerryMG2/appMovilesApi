70eeedd49a0afe164a6221ee3ebdf21c7ceb4732
GerryMG <gerardo.andresz4@live.com>
2020-07-08 19:09:13 -0600

    changes

--- routes/payment.js
+++ routes/payment.js
@@ -5,6 +5,8 @@ const payment = require("../controllers/paymentController");
 
 router.get("/stripe-key",authMovil,payment.getkey);
 router.post("/pay",authMovil,payment.pay);
-
+router.get("/",(req,res)=>{
+    res.redirect("/admin/module")
+})
 
 module.exports = router;
