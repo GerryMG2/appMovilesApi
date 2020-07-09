4b91badcc437799633080c3f650b502370c4de6b
GerryMG <gerardo.andresz4@live.com>
2020-07-09 03:08:37 -0600

    policy

--- routes/payment.js
+++ routes/payment.js
@@ -7,6 +7,12 @@ router.get("/stripe-key",authMovil,payment.getkey);
 router.post("/pay",authMovil,payment.pay);
 router.get("/",(req,res)=>{
     res.redirect("/admin/module")
-})
+});
+router.get("/policy",(req,res)=>{
+    res.render("webApp/policy.pug",{});
+});
+router.get("/terms",(req,res)=>{
+    res.render("webApp/terms.pug",{});
+});
 
 module.exports = router;
